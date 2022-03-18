0c2b535f6d2623740bba1195315303582d5633a5
Thiago Benega <thiagobenega@gmail.com>
2022-03-18 17:06:51 -0300

    test: ensure Login present valid state if password is valid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Styles from './login-styles.scss'
-import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
+import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
 
@@ -24,17 +24,17 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
       emailError: validation.validate('email', state.email),
       passwordError: validation.validate('password', state.password)
     })
-  }, [state.email])
+  }, [state.email, state.password])
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={{ state, setState }}>
+      <Context.Provider value={ { state, setState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
-          <button data-testid="submit" disabled type="submit" className={Styles.submit}>Entrar</button>
+          <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
           <span className={Styles.link}>Criar conta</span>
           <FormStatus />
         </form>
