d6fd9e85fd4bcb8050cb4ce33e414890ab8f2797
Thiago Benega <thiagobenega@gmail.com>
2022-03-18 12:35:58 -0300

    feat: ensure login calls validation with correct email

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
+import { Validation } from '@/presentation/protocols/validation'
+
+class ValidationLogin implements Validation {
+  validate (input: object): string {
+    return ''
+  }
+}
 
 const Router: React.FC = () => {
+  const validation = new ValidationLogin()
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login validation={validation}/>} />
       </Routes>
     </BrowserRouter>
   )
