252be9c99ed5ad6228e08742d82e4a8c9af63769
Thiago Benega <thiagobenega@gmail.com>
2022-03-18 19:31:10 -0300

    refactor: move AuthenticationSpy to its own file

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,30 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
-import { Validation } from '@/presentation/protocols/validation'
-import { Authentication, AuthenticationParams } from '@/domain/usecases'
-import { AccountModel } from '@/domain/models'
-
-class ValidationLogin implements Validation {
-  validate (fieldName: string, fieldValue: string): string {
-    return ''
-  }
-}
-
-class AuthenticationLogin implements Authentication {
-  async auth (params: AuthenticationParams): Promise<AccountModel> {
-    return Promise.resolve(null)
-  }
-}
 
 const Router: React.FC = () => {
-  const validation = new ValidationLogin()
-  const authentication = new AuthenticationLogin()
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login validation={validation} authentication={authentication}/>} />
+        <Route path='/login' element={Login} />
       </Routes>
     </BrowserRouter>
   )
