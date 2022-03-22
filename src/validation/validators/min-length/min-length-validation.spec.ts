e75cc923c76f810d816e4a4448e59cbf8ce62829
Thiago Benega <thiagobenega@gmail.com>
2022-03-22 16:49:22 -0300

    feat: implementing MinLengthValidation

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,19 @@
+import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
+import { MinLengthValidation } from './min-length-validation'
+
+const makeSut = (minLength: number): MinLengthValidation => new MinLengthValidation(faker.database.column(), minLength)
+
+describe('MinLengthValidation', () => {
+  test('should return error if value is invalid', () => {
+    const sut = makeSut(5)
+    const error = sut.validate(faker.random.alphaNumeric(3))
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('should return falsy if value is valid', () => {
+    const sut = makeSut(5)
+    const error = sut.validate(faker.random.alphaNumeric(5))
+    expect(error).toBeFalsy()
+  })
+})
